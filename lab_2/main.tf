e47b383fc2aa11adfb54ae640d9a8bee0b52307e
Philips Benjamin <philips.benjamin@softchoice.com>
2020-07-26 00:06:09 -0400

    update lb module source

--- lab_2/main.tf
+++ lab_2/main.tf
@@ -63,7 +63,7 @@ module "managed_instance_group" {
 
 module "load_balancer_default" {
   name         = "basic-load-balancer-default"
-  source       = "../../"
+  source       = "GoogleCloudPlatform/lb/google"
   region       = var.region
   service_port = 80
   network      = google_compute_network.network.name
@@ -73,7 +73,7 @@ module "load_balancer_default" {
 
 module "load_balancer_no_hc" {
   name                 = "basic-load-balancer-no-hc"
-  source               = "../../"
+  source               = "GoogleCloudPlatform/lb/google"
   region               = var.region
   service_port         = 80
   network              = google_compute_network.network.name
@@ -84,7 +84,7 @@ module "load_balancer_no_hc" {
 
 module "load_balancer_custom_hc" {
   name         = "basic-load-balancer-custom-hc"
-  source       = "../../"
+  source       = "GoogleCloudPlatform/lb/google"
   region       = var.region
   service_port = 8080
   network      = google_compute_network.network.name
